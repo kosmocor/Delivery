685b10fc90c2d15cdbb4b0b92cb3030632634d00
kosmocor <kosmocor@gmail.com>
2020-10-29 08:42:10 +0900

    kafka group : hotelimage -> accommodation

--- src/main/java/accommodation/Delivery.java
+++ src/main/java/accommodation/Delivery.java
@@ -57,7 +57,8 @@ public class Delivery {
             deliveryRequested.publishAfterCommit();
              */
 
-            long delayed = (long) (400 + Math.random() * 300);
+            //long delayed = (long) (400 + Math.random() * 300);
+            long delayed = 0;
             try {
                 //Thread.currentThread().sleep((long) (400 + Math.random() * 300));
                 Thread.currentThread().sleep((long) delayed);
